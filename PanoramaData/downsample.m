1a572d002f84e5a2eba62982b279036fd4be8d00
zxh <2213562189@qq.com>
2017-02-22 21:23:06 +0800

    no message

--- PanoramaData/downsample.m
+++ PanoramaData/downsample.m
@@ -2,5 +2,5 @@
 [FileName,PathName] = uigetfile('*.*','Select the image');  
 Im=imread([PathName FileName]);
 DSimg=Im(1:4:end,1:4:end,:);
-FileName(end-4:end)=[];
+FileName(end-3:end)=[];
 imwrite(DSimg,strcat(FileName,'_DS','.jpg'));
